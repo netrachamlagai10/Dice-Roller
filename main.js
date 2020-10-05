1fa8870175de604817b22a93ead866fd88beddbe
Netra Chamlagai <nayan@Netras-MacBook-Pro.local>
2020-10-05 16:05:47 -0400

    add file

---
+++ main.js
@@ -0,0 +1,28 @@
+let roll = document.querySelector(".Roll");
+let totalresult = document.querySelector("#total");
+let showtherolls = document.querySelector(".showtherolls");
+let lists = document.querySelector("#lists");
+let numofdice = document.querySelector("#number-of-dice")
+let dierolls = [];
+let counter = 0;
+roll.addEventListener("click", function(){
+let dicenum = numofdice.value
+let total = 0;
+while (counter<dicenum) {
+    let x = Math.floor(Math.random()*6 +1)
+    dierolls.push(x)
+    total +=x
+    counter++
+    
+}
+totalresult.innerHTML=total
+})
+
+showtherolls.addEventListener("click", function(){
+    str = "<ol>";
+    dierolls.forEach(function (dicenum){
+        str += "<li>" + dicenum + "</li>";
+    });
+    str +="</ol>";
+    lists.innerHTML=str
+});
