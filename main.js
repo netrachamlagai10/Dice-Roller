f1d4899b27b53283539178679ec838ef9af3e5ff
Netra Chamlagai <nayan@Netras-MacBook-Pro.local>
2020-10-06 13:02:05 -0400

    Reset button add

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ let roll = document.querySelector(".Roll");
 let totalresult = document.querySelector("#total");
 let showtherolls = document.querySelector(".showtherolls");
 let lists = document.querySelector("#lists");
-let numofdice = document.querySelector("#number-of-dice")
+let numofdice = document.querySelector("#number-of-dice");
+let numberofresets = document.querySelector("#reset-button");
 let dierolls = [];
 let counter = 0;
 roll.addEventListener("click", function(){
-let dicenum = numofdice.value
+let dicenum = numofdice.value 
 let total = 0;
 while (counter<dicenum) {
     let x = Math.floor(Math.random()*6 +1)
@@ -26,3 +27,10 @@ showtherolls.addEventListener("click", function(){
     str +="</ol>";
     lists.innerHTML=str
 });
+numberofresets.addEventListener("click", function() {
+    
+    totalresult.innerHTML = 0;
+    numofdice.value =0;
+    lists.innerHTML = '';
+    
+})
